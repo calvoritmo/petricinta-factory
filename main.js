299cde0bf0db944f14c33fc870902b5eb6eab6f3
calvoritmo <105608815+calvoritmo@users.noreply.github.com>
2026-06-03 21:56:58 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import { setup as setupCounter } from './counter.js';
+import { setup as setupIronDrill } from './ironDrill.js';
+import { setup as setupCoalDrill } from './coalDrill.js';
+
+setupCounter();
+setupIronDrill();
+setupCoalDrill();
